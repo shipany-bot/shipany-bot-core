fc9fdec516f04f430dcee4c1e871e461305c12ec
Sergey Parfenyuk <Sergey.Parfenyuk@gmail.com>
2024-05-25 16:29:42 +0200

    Add hard-coded set of secrets

--- src/shipany/bot/contrib/aiogram/context.py
+++ src/shipany/bot/contrib/aiogram/context.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
 
+from typing import Mapping, MutableMapping
+
 from aiogram import types as aiogram_types
 from pydantic import BaseModel, Field
 
 
+def _collect_secrets() -> Mapping[str, str]:
+  return {"openai_token": "t0k3n"}
+
+
 class Context(BaseModel):
-  captures: dict[str, str] = Field(default_factory=dict)
+  captures: MutableMapping[str, str] = Field(default_factory=dict)
+  secrets: Mapping[str, str] = Field(frozen=True, default_factory=_collect_secrets)
   event: aiogram_types.TelegramObject
 
 
